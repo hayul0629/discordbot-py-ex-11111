c3883a2cee8994b2e791c408c113c466a28451be
hayul0629 <120244649+hayul0629@users.noreply.github.com>
2023-03-13 22:17:11 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -96,7 +96,7 @@ async def on_message(message):
         except asyncio.TimeoutError:
             await message.channel.send("Time out error")
         else:
-         await msg.delete()
+         await message.delete()
 try:
     client.run(TOKEN)
 except discord.errors.LoginFailure as e:

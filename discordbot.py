6efdaae156ad603c5a2ba8499edb5723342937f7
hayul0629 <120244649+hayul0629@users.noreply.github.com>
2023-03-15 22:13:00 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -62,8 +62,8 @@ async def on_message(message):
     try:
         reaction1, user = await bot.wait_for('reaction_add', timeout=60.0, check=check1)
 
-    if reaction2.emoji == '🥤': # 탄산 선택
-        await message.channel.send('탄산을 선택하셨습니다. 어떤 음료를 선택하시겠습니까?\n:one: 콜라\n:two: 사이다\n:three: 환타\n')
+        if reaction2.emoji == '🥤': # 탄산 선택
+            await message.channel.send('탄산을 선택하셨습니다. 어떤 음료를 선택하시겠습니까?\n:one: 콜라\n:two: 사이다\n:three: 환타\n')
 
         def check2(reaction, user):
             return user != client.user and str(reaction.emoji) in ['1️⃣', '2️⃣', '3️⃣']

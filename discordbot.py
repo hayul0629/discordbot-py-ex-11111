7a7828dbb6f1552d5dd71453920d2fb323b4cba6
hayul0629 <120244649+hayul0629@users.noreply.github.com>
2023-03-16 22:38:08 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -56,29 +56,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-class MyView(discord.ui.View):
-    @discord.ui.select( # the decorator that lets you specify the properties of the select menu
-        placeholder = "Choose a Flavor!", # the placeholder text that will be displayed if nothing is selected
-        min_values = 1, # the minimum number of values that must be selected by the users
-        max_values = 1, # the maximum number of values that can be selected by the users
-        options = [ # the list of options from which users can choose, a required field
-            discord.SelectOption(
-                label="Vanilla",
-                description="Pick this if you like vanilla!"
-            ),
-            discord.SelectOption(
-                label="Chocolate",
-                description="Pick this if you like chocolate!"
-            ),
-            discord.SelectOption(
-                label="Strawberry",
-                description="Pick this if you like strawberry!"
-            )
-        ]
-    )
 
-    if message.content == '!sample':
-        await ctx.send("Choose a flavor!", view=MyView())
 ##########################################################################################################
     if message.content == 'sample':
         sent_message = await message.channel.send('test sample')

e11c73d70132606018086d6b6fa33d78f0dcfe7e
hayul0629 <120244649+hayul0629@users.noreply.github.com>
2023-03-17 07:38:44 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -75,7 +75,7 @@ async def on_reaction_add(reaction, user):
     if reaction.emoji == '😎':
         sent_message = reaction.message
         await reaction.message.clear_reactions()
-        await sent_message.reply('click any things!')
+        await message.edit(content="click anything!")
         await sent_message.add_reaction('❤️')
         await sent_message.add_reaction('🤍')
 
@@ -85,18 +85,18 @@ async def on_reaction_add(reaction, user):
         
     if reaction.emoji == '🤍':
         sent_message = reaction.message
-        await sent_message.reply('white heart!')
+        await message.edit(content="white heart!")
         await reaction.message.clear_reactions()
         await sent_message.add_reaction('1️⃣')
         await sent_message.add_reaction('2️⃣')
         
     if reaction.emoji == '1️⃣':
         sent_message = reaction.message
-        await sent_message.reply('1')
+        await message.edit(content="1")
         
     if reaction.emoji == '2️⃣':
         sent_message = reaction.message
-        await sent_message.reply('2')
+        await message.edit(content="1")
                
         
         

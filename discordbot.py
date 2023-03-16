e72e864ca8a60e912dce50d731c326ae1c2c0c91
hayul0629 <120244649+hayul0629@users.noreply.github.com>
2023-03-16 22:29:51 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,9 +5,6 @@ import asyncio
 import random
 from time import sleep
 from dotenv import load_dotenv
-import sys
-sys.path.insert(1, './lib')
-from discord_components import DiscordComponents, Button, ButtonStyle, InteractionType
 from discord.ext import commands
 
 import os
@@ -59,26 +56,31 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!test'):
-        await message.channel.send(
-            "test button",
-            components = [
-                Button(style=ButtonStyle.blue, label="test", id="test_button")
-            ]
-        )
+class MyView(discord.ui.View):
+    @discord.ui.select( # the decorator that lets you specify the properties of the select menu
+        placeholder = "Choose a Flavor!", # the placeholder text that will be displayed if nothing is selected
+        min_values = 1, # the minimum number of values that must be selected by the users
+        max_values = 1, # the maximum number of values that can be selected by the users
+        options = [ # the list of options from which users can choose, a required field
+            discord.SelectOption(
+                label="Vanilla",
+                description="Pick this if you like vanilla!"
+            ),
+            discord.SelectOption(
+                label="Chocolate",
+                description="Pick this if you like chocolate!"
+            ),
+            discord.SelectOption(
+                label="Strawberry",
+                description="Pick this if you like strawberry!"
+            )
+        ]
+    )
+    async def select_callback(self, select, interaction): # the function called when the user is done selecting options
+        await interaction.response.send_message(f"Awesome! I like {select.values[0]} too!")
 
-@client.event
-async def on_button_click(interaction):
-    if interaction.component.id == "test_button":
-        await interaction.respond(
-            content="button clicked!",
-            type=InteractionType.ChannelMessageWithSource
-        )
-
-@client.event
-async def on_button_click(res):
-    if res.component.label == 'test':
-        await res.respond(content='this is test ans!')
+    if message.content == '!sample':
+        await ctx.send("Choose a flavor!", view=MyView())
 ##########################################################################################################
     if message.content == 'sample':
         sent_message = await message.channel.send('test sample')

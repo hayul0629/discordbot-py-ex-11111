e1210b5af964617a75c9354d5a72729c0c8b7191
hayul0629 <120244649+hayul0629@users.noreply.github.com>
2023-03-16 20:23:00 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,58 +1,13 @@
-from cmath import log
-from distutils.sysconfig import PREFIX
-import discord
-import asyncio
-import random
-from time import sleep
-from dotenv import load_dotenv
-from discord.ext import commands
 import os
-load_dotenv()
+import discord
+from discord.ext import commands
 
-PREFIX = os.environ['PREFIX']
-TOKEN = os.environ['TOKEN']
-VGEN = ['ghufranad:dedek2006',
-'unluckyme1:madhuroy1',
-'venitastah:15aug1995',
-'arunsuman:Drag@123',
-'2ez4chano:Sagitarius1220',
-'henhenhen12:S@ms^ng123',
-'xoprishix:rras1975',
-'mathon07:sheilarevilla030720143',
-'prantoislam:Pr@nto1208217',
-'killxsoul:01091994zaza',
-'haymanns:haymann66318',
-'rezzatiey:Rez240700@',
-'boilingwaters:ainalatosa08',
-'ts547878:ts86520000',
-'darkark404:Katasandi1234',
-'devilarfter:Devil981577625',
-'theeaszx486584:!.DekxzaqZeRt$0Cool#@',
-'xkrat0sxx:kratos1481',
-'xpapiemz:Mimidada9',
-'whiteomega98:kobik123456789',
-'jinnn07:Ap10aw2525',
-'hortenzy:boomgunpap08',
-'skyskysora88:cat1314520',
-'drian082219:123Defender-1',
-'zekairyuken:zekairyuken13',
-'thickka:quocanh30032005',
-'loligragon:Aum!Loli@0877349590',
-'urtywer:urtywer@leage1',
-'p33carryu:Nott064224',
-'h4wkk1ll3r:sankalp12']
-
-points = {}
-client = discord.Client()
+client = commands.Bot(command_prefix=os.environ['PREFIX'])
 
 @client.event
 async def on_ready():
-    print(f'Logged in as {client.user}.')
+    print('Bot is ready.')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -62,9 +17,9 @@ async def on_message(message):
         sent_message = await message.channel.send('test sample')
         await sent_message.add_reaction('😎')
 
-    await bot.process_commands(message)
+    await client.process_commands(message)
 
-@bot.event
+@client.event
 async def on_reaction_add(reaction, user):
     if user.bot:
         return
@@ -77,21 +32,7 @@ async def on_reaction_add(reaction, user):
 
     if reaction.emoji == '❤️':
         sent_message = reaction.message
-        await sent_message.reply('heart!')
-
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        await sent_message.reply('heart!')     
 try:
     client.run(TOKEN)
 except discord.errors.LoginFailure as e:

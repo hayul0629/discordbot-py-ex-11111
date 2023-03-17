80aa3585429d38a495e637bae182af0c175f6d04
hayul0629 <120244649+hayul0629@users.noreply.github.com>
2023-03-17 20:45:16 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -58,7 +58,32 @@ async def on_message(message):
     if message.channel.category_id == 1078628991969267802 and message.content == '안녕':
         await message.channel.send('안녕하세요')
         
-        
+    if message.content.startswith('!point'):
+        # !point 명령어를 입력한 사용자의 ID와 포인트 값을 가져옴
+        author_id = str(message.author.id)
+        author_points = get_points(author_id)
+
+        # 가져온 포인트 값을 메시지로 보냄
+        await message.channel.send(f'{message.author.mention}님의 현재 포인트: {author_points}')
+
+    elif message.content.startswith('!p'):
+        # !p 명령어를 입력한 사용자의 ID와 입력한 값을 가져옴
+        author_id = str(message.author.id)
+        args = message.content.split(' ')
+        points_to_add = int(args[1])
+        target_user_mention = args[2]
+
+        # 메시지에서 언급한 사용자의 ID를 가져옴
+        target_user_id = get_user_id_from_mention(target_user_mention)
+
+        # 언급한 사용자가 존재하지 않을 경우 오류 메시지를 보냄
+        if target_user_id is None:
+            await message.channel.send(f'{target_user_mention} 사용자를 찾을 수 없습니다.')
+            return
+
+        # 포인트 값을 업데이트하고, 메시지를 보냄
+        add_points(target_user_id, points_to_add)
+        await message.channel.send(f'{target_user_mention} 사용자에게 {points_to_add} 포인트가 추가되었습니다.')
         
     if message.content.startswith('!sample'):
         global sent_message

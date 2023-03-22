846594574b5ac183fcf405e45eda43c02adf351d
hayul0629 <120244649+hayul0629@users.noreply.github.com>
2023-03-22 22:31:59 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -159,14 +159,12 @@ async def on_message(message):
                 await message.channel.send(embed=embedVar22)
                 await message.channel.send(embed=embedVar24)
                 await message.author.send(random.choice(allac))
-                await message.delete()
             else:
                 embedVar23 = discord.Embed(title="작액이 부족합니다.", color=0xff1100)
                 embedVar23.add_field(name="",value="<#1078652866165743676>에서 `.bu-v`명령어로 콘 충전 가능합니다.",inline=False)
                 embedVar25 = discord.Embed(title="잔액", color=0xff1100)
                 await message.channel.send(embed=embedVar23)
                 await message.channel.send(embed=embedVar25)
-                await message.delete()
 
 
     
